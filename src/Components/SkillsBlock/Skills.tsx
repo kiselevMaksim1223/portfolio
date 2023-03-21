27122997a162d3a3ca748a34e4d9da068720fd16
Maksim_Kiselev <laurkurt@gmail.com>
2023-03-21 23:13:47 +0300

    skill block kind of done

--- src/Components/SkillsBlock/Skills.tsx
+++ src/Components/SkillsBlock/Skills.tsx
@@ -2,21 +2,36 @@ import React from 'react';
 import s from "./Skills.module.css"
 import comStyles from "../../Common/Container/ContainerStyle.module.css"
 import {SkillItem} from "./skillItem/SkillItem";
+import {Title} from "../../Common/Title/Title";
+import { TbBrandJavascript } from 'react-icons/tb';
+import { TbBrandHtml5 } from 'react-icons/tb';
+import { TbBrandCss3 } from 'react-icons/tb';
+import { TbBrandReact } from 'react-icons/tb';
+import { TbBrandRedux } from 'react-icons/tb';
+import { TbBrandTypescript } from 'react-icons/tb';
 
 export const Skills = () => {
     const skills = [
-        {id:1, title:"HTML", description:"hfsaujdflsakdjl sdls;a dksaldnakjfh ndsla kvmdaklvmlsv"},
-        {id:2, title:"CSS", description:"dskdmksjafh dskbvnjdskm cjdskbvci usdnvuildskjciosalkcsoaml;ck;s"},
-        {id:3, title:"JS", description:"dsfndbsyiufvjiaufsj dvuifhd vnaioslkdj vnufidkdiusjkvovjdshiklvnsdolcxv"},
-        {id:4, title:"JS", description:"dfhsdak jvdshavjhcdksv nkdmckd smkdsjf kdjsfkdsjf kdsjsiruekvmkcn bjdhgjsdnjjfif ja k;LDKSJFIEUWJM;dkmv cksnbio jdikjdiofs"},
+        {id:1, title:"HTML", description:"hfsaujdflsakdjl sdls;a dksaldnakjfh ndsla kvmdaklvmlsv", icon:<TbBrandHtml5 size={"60px"} color={"#ccd6f6"} />},
+        {id:2, title:"CSS", description:"dskdmksjafh dskbvnjdskm cjdskbvci usdnvuildskjciosalkcsoaml;ck;s", icon:<TbBrandCss3 size={"60px"} color={"#ccd6f6"}/> },
+        {id:3, title:"JS", description:"dsfndbsyiufvjiaufsj dvuifhd vnaioslkdj vnufidkdiusjkvovjdshiklvnsdolcxv", icon: <TbBrandJavascript size={"60px"} color={"#ccd6f6"}/>},
+        {id:4, title:"React", description:"dfhsdak jvdshavjhcdksv nkdmckd smkdsjf kdjsfkdsjf kdsjsiruekvmkcn bjdhgjsdnjjfif ja k;LDKSJFIEUWJM;dkmv cksnbio jdikjdiofs", icon:<TbBrandReact size={"60px"} color={"#ccd6f6"}/> },
+        {id:5, title:"Redux", description:"dfhsdak jvdshavjhcdksv nkdmckd smkdsjf kdjsfkdsjf kdsjsiruekvmkcn ", icon:<TbBrandRedux size={"60px"} color={"#ccd6f6"}/> },
+        {id:6, title:"TS", description:"dfhsdak jvdshavjhcdksv nkdmckd smkdsjf kdjsfkdsjf kdsjsiruekvmkcn ", icon:<TbBrandTypescript size={"60px"} color={"#ccd6f6"}/> },
     ]
     return (
         <div className={s.wrapper}>
             <div className={comStyles.container}>
                 <div className={s.skillsBlock}>
-                    <h2 className={s.title}>Skills</h2>
+
+                    <Title text={"Skills"}/>
                     <div className={s.skills}>
-                        {skills.map(si => <SkillItem key ={si.id} title={si.title} description={si.description}/>)}
+                        {skills.map(si => {
+                            return(
+                            <SkillItem key={si.id} title={si.title} description={si.description}>
+                                {si.icon}
+                            </SkillItem>)
+                        })}
                     </div>
                 </div>
             </div>
--- src/Components/SkillsBlock/skillItem/SkillItem.tsx
+++ src/Components/SkillsBlock/skillItem/SkillItem.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import s from "./SkillItem.module.css"
+
 
 type skillItemType ={
     title:string
     description:string
+    children:ReactNode
 }
 
 export const SkillItem = (props:skillItemType) => {
     return (
         <div className={s.skillItem}>
-            <div className={s.icon}></div>
+            <div className={s.icon}>{props.children}</div>
             <h3 className={s.skillTitle}>{props.title}</h3>
             <span className={s.description}>{props.description}</span>
         </div>
